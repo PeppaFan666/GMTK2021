208296fa8934d49106db9ba84ee4463176c910ee
PeppaFan666 <treebarker2007@gmail.com>
2021-06-11 13:55:45 -0600

    add entity

---
+++ Entity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GMTK2021
+{
+    class Entity
+    {
+        Rectangle hitbox;
+        public virtual void SetDefaults() { }
+        public virtual void OnLoad() { }
+        public bool Colliding(Rectangle targethitbox)
+        {
+            return this.hitbox.Intersects(targethitbox);
+        }
+    }
+}
