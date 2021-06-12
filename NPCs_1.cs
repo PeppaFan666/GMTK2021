e21817f2c481f6a6eea32293751b1411d61a95a2
HenryBass <63601449+HenryBass@users.noreply.github.com>
2021-06-12 08:55:09 -0400

    Added Content

---
+++ NPCs_1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GMTK2021
+{
+    class NPCs_1 : NPC
+    {
+        float xPos = 1;
+        int speed = 1;
+
+        public override void SetDefaults() // Floating Eye Enemy
+        {
+            center = new Vector2(20, 20);
+        }
+        public override void AI()
+        {
+            xPos += speed;
+            center += new Vector2(xPos, 0);
+
+            if (xPos == 5)
+            {
+                speed = -1;
+            }
+        }
+    }
+}
